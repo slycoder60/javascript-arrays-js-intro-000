d2b1f38edbc953773a6a088dac6e8a8177e433f9
Omar Farah <farah.omar8787@gmail.com>
2020-02-16 19:28:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
 chocolateBars = ['snickers', 'hundred grand', 'kitkat','skittles']
+
+addElementToBeginningOfArray function(Burgers= ['Chicken Burgers', 'Camal Burgers']){
+  return [...Burgers,'Ham Burgers']
+
+}
