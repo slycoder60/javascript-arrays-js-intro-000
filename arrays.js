d364b27392a684a657adf934362766199938374d
Omar Farah <farah.omar8787@gmail.com>
2020-02-16 20:32:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,5 +19,6 @@ oddNumbers.push('foo')
   return oddNumbers
 }
 
-function accessElementInArray(familyMembers['mom', 'dad','brother'])
+function accessElementInArray(familyMembers['mom', 'dad','brother']){
 return familyMembers([0])
+}
